28ad6a70f195a7ce2392a6ea354153eba1b8f283
Scott Numamoto <scottnuma@users.noreply.github.com>
2020-03-22 16:35:05 -0700

    Fix tests (#11)
    - Switch to using pytest
    - Update GitHub Actions
    - Use Makefile to run tests

--- mentor_matching/mentor_test.py
+++ mentor_matching/mentor_test.py
@@ -1,36 +1,28 @@
 import csv
 import unittest
 
-from utils import Mentor
+from mentor_matching.utils import Mentor
 
 
-class TestParsingMentor(unittest.TestCase):
-    def test_from_string(self):
-        raw_csv_line = [
-            "Mavericl,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,,,Jessica,1,Inconvenient,Convenient,Convenient,Not possible,Not possible,Somewhat,Very Confident"
-        ]
-        reader = csv.reader(raw_csv_line)
-        processed_line = next(reader)
-        mentor = Mentor(processed_line)
+def test_from_string():
+    raw_csv_line = [
+        "Mavericl,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,,,Jessica,1,Inconvenient,Convenient,Convenient,Not possible,Not possible,Somewhat,Very Confident"
+    ]
+    reader = csv.reader(raw_csv_line)
+    processed_line = next(reader)
+    mentor = Mentor(processed_line)
 
-        self.assertEqual(mentor.name, "Mavericl")
-        self.assertEqual(mentor.teamTypeRequests, [1, 1, 0, 0])
-        self.assertEqual(mentor.teamsRequested, [])
-        self.assertEqual(mentor.teamsRequired, [])
-        self.assertEqual(mentor.mentorsRequired, ["Jessica"])
-        self.assertEqual(mentor.comfortAlone, "1")
-        self.assertEqual(
-            mentor.transitConveniences,
-            [
-                "Inconvenient",
-                "Convenient",
-                "Convenient",
-                "Not possible",
-                "Not possible",
-            ],
-        )
-        self.assertEqual(mentor.skillsConfidence, ["Somewhat", "Very Confident"])
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert mentor.name == "Mavericl"
+    assert mentor.teamTypeRequests == [1, 1, 0, 0]
+    assert mentor.teamsRequested == []
+    assert mentor.teamsRequired == []
+    assert mentor.mentorsRequired == ["Jessica"]
+    assert mentor.comfortAlone == "1"
+    assert mentor.transitConveniences == [
+        "Inconvenient",
+        "Convenient",
+        "Convenient",
+        "Not possible",
+        "Not possible",
+    ]
+    assert mentor.skillsConfidence == ["Somewhat", "Very Confident"]
