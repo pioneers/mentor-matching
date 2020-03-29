67afb688279944f9151ce866d97b3e83f872bea0
Scott Numamoto <snumamoto@lyft.com>
2020-03-28 18:22:15 -0700

    Improve data

--- mentor_matching/mentor_test.py
+++ mentor_matching/mentor_test.py
@@ -6,17 +6,17 @@ from mentor_matching.mentor import Mentor
 
 def test_from_string():
     raw_csv_line = [
-        "Mavericl,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,,,Jessica,1,Inconvenient,Convenient,Convenient,Not possible,Not possible,Somewhat,Very Confident"
+        "Primeape,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,,,Starmie,1,Inconvenient,Convenient,Convenient,Not possible,Not possible,Somewhat,Very Confident"
     ]
     reader = csv.reader(raw_csv_line)
     processed_line = next(reader)
     mentor = Mentor.from_list(processed_line)
 
-    assert mentor.name == "Mavericl"
+    assert mentor.name == "Primeape"
     assert mentor.teamTypeRequests == [1, 1, 0, 0]
     assert mentor.teamsRequested == []
     assert mentor.teamsRequired == []
-    assert mentor.mentorsRequired == ["Jessica"]
+    assert mentor.mentorsRequired == ["Starmie"]
     assert mentor.comfortAlone == constants.comfortAloneLevels[0]
     assert mentor.transitConveniences == [
         "Inconvenient",
